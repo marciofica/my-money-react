7744b4654816af71f6bacb03dbf9c7440ff4fbf4
marciofica@gmail.com <marciofica@gmail.com>
2019-10-29 21:14:11 -0300

    hooks personalizados, reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import useGet from './useGet'
 
 function App() {
+  const data = useGet('https://mymoney-fa461.firebaseio.com/movimentacoes.json')
   return (
     <div>
       <h1>My Money</h1>
+      {JSON.stringify(data.data)}
+      {data.loading && <p>Carregando...</p>}
     </div>
   );
 }
