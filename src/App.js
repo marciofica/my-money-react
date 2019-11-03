4ccb43dfa1e6aad05162feec6359dd4ceb6f7962
marciofica@gmail.com <marciofica@gmail.com>
2019-11-03 19:30:22 -0300

    hooks personalizados

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react'
-import useGet from './useGet'
+import Rest from './rest'
+
+const baseUrl = 'https://mymoney-fa461.firebaseio.com'
+
+const {useGet, usePost, useDelete} = Rest(baseUrl)
+
 
 function App() {
-  const data = useGet('https://mymoney-fa461.firebaseio.com/movimentacoes.json')
+  const data = useGet('movimentacoes/2019-08')
+  const [postData, post] = usePost('movimentacoes/2019-08')
+  const [deleteData, remove] = useDelete()
+  
+  const saveNew = () => {
+    post({valor: 10, descricao: 'Olá'})
+  }
+
+  const doRemove = () => {
+    remove('movimentacoes/2019-08/-LsnEm-mm_CRzQudh1iu')
+  }
+
   return (
     <div>
       <h1>My Money</h1>
       {!data.loading && JSON.stringify(data.data)}
       {data.loading && <p>Carregando...</p>}
+      <button onClick={saveNew}>Salvar</button>
+      <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={doRemove}>Remover</button>
+      <pre>{JSON.stringify(deleteData)}</pre>
     </div>
   );
 }
