6d905060ad592a13eed025dd1ff937e2722d106d
marcio.figueiredo <marciofica@gmail.com>
2019-11-07 15:14:02 -0300

    Ajuste layout movimentações

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './elements/Header'
 import Home from './pages/Home'
-import Movimentacoes from './Movimentacoes';
+import Movimentacoes from './pages/Movimentacoes';
 
 function App() {
 
