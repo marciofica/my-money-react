792d7fa7cb555975214a8d8c2b1739471802ddec
marciofica@gmail.com <marciofica@gmail.com>
2019-11-10 21:11:48 -0300

    ajuste tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {Redirect} from 'react-router-dom'
 import {usePost} from '../utils/rest'
+import './Login.css'
 
 const baseUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDav6jiqtwacCsPjiVlNZ-hPukTKx_sy-I'
 
@@ -22,7 +23,8 @@ const Login = () => {
             setLogado(true)
         }
     })
-    const login = async() => {
+    const login = async(evt) => {
+        evt.preventDefault()
         await signin({
             email,
             password: senha,
@@ -43,15 +45,17 @@ const Login = () => {
         return <Redirect to='/' />
     }
     return(
-        <div>
-            <h1>Login</h1>
-            {
-                (postData.error && postData.error.length > 0) &&
-                <p>E-mail ou senha incorreto!</p>
-            }
-            <input type='text' value={email} onChange={onChangeEmail} />
-            <input type='password' value={senha} onChange={onChangeSenha} />
-            <button onClick={login}>Login</button>
+        <div className='text-center'>
+            <form className='form-signin'>
+                <h1 className='h3 mb-3 font-weight-normal'>Faça login</h1>
+                {
+                    (postData.error && postData.error.length > 0) &&
+                    <p>E-mail ou senha incorreto!</p>
+                }
+                <input className='form-control' type='text' value={email} onChange={onChangeEmail} />
+                <input className='form-control mb-3 mt-2' type='password' value={senha} onChange={onChangeSenha} />
+                <button className='btn btn-lg btn-primary btn-block' onClick={login}>Login</button>
+            </form>
         </div>
     )
 }
